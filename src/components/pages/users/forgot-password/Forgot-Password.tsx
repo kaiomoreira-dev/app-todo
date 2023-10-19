7276f4f9c5c71d414382f7454b41e42b86d7d8e4
Kaio Moreira <kaio-dev@outlook.com>
2023-10-19 10:50:08 -0300

    create: new message for alert send email for refinitions password

--- src/components/pages/users/forgot-password/Forgot-Password.tsx
+++ src/components/pages/users/forgot-password/Forgot-Password.tsx
@@ -2,7 +2,11 @@ import { ChangeEvent, FormEvent, useState } from 'react'
 import { Footer } from '../../../footer/Footer'
 import { Header } from '../../../header/Header'
 import styles from './Forgot-Password.module.css'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import rocketImg from '../../../../assets/rocket.svg'
+import todoImg from '../../../../assets/todo.svg'
+import sendEmailImg from '../../../../assets/send-email.png'
+
 
 interface IEmail{
     email: string,
@@ -12,6 +16,10 @@ export function ForgotPassword(){
     const [emailField, setEmailField] = useState<IEmail>({
         email: '',
     } as IEmail)
+
+    const [message, setMessage] = useState<boolean>(false)
+
+    const navigate = useNavigate()
 
     async function handleForgotPassword(event: FormEvent<HTMLFormElement>){
         try {
@@ -31,9 +39,25 @@ export function ForgotPassword(){
             setEmailField({
                 email: '',
             })
+            setMessage(true)
+
+            redirect().then((result)=>{
+                if (result) {
+                    navigate('/login')
+                }
+            })
+
         } catch (error) {
             alert('E-mail não encontrado')
         }
+    }
+
+    async function redirect(){
+        return new Promise((resolve) => {
+            setTimeout(()=>{
+            resolve(true)
+            }, 5000)
+        })
     }
 
     //[x] criar metodo para receber os dados do formulário
@@ -50,7 +74,22 @@ export function ForgotPassword(){
         <div className={styles.container}>
             <Header/>
             <main className={styles['main-content']}>
-                <form action="" onSubmit={handleForgotPassword}>
+                <div className={message ? styles['confirm-email'] : styles.none}>
+                        <img src={sendEmailImg} alt="" />
+
+                        <span>Redefinição Enviada com Sucesso!</span>
+                        <p>
+                            Um e-mail com instruções para redefinir sua senha foi enviado para o seu endereço de e-mail. Por favor, verifique sua caixa de entrada e siga as instruções fornecidas. 
+                        </p>
+                </div>
+                
+                <span className={message ? styles.icon : styles.none}>
+                    Equipe
+                    <img src={rocketImg} alt="" />
+                    <img src={todoImg} alt="" />
+                </span>
+
+                <form id={message ? styles.none : ''} action="" onSubmit={handleForgotPassword}>
                     <fieldset>
                     <div className={styles['title-content']}>
                         <span>Redefinição de senha</span>
@@ -62,11 +101,9 @@ export function ForgotPassword(){
                     <input id={styles['space-down']}type="text" value={emailField.email} onChange={handleOnChange}  name="email" placeholder='example@email.com' />
                     </fieldset>
                     <footer>
-                        
                         <Link to="/login">
                             <button type='button'>Voltar</button>
                         </Link>
-                        
                         <button type='submit'>Pesquisar</button>
                     </footer>
                 </form>
