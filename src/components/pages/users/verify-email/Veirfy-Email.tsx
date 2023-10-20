111b3fce7b3047d11edd454d5ade3f5872e4525f
Kaio Moreira <kaio-dev@outlook.com>
2023-10-20 12:05:55 -0300

    alter: message when verify account and remove: icone for message when register

--- src/components/pages/users/verify-email/Veirfy-Email.tsx
+++ src/components/pages/users/verify-email/Veirfy-Email.tsx
@@ -8,7 +8,7 @@ import { useLocation, useNavigate } from 'react-router';
 import { useEffect, useState } from 'react';
 
 export function VerifyEmail(){
-    const [block, setBlock] = useState<boolean>(true)
+    const [block, setBlock] = useState<boolean>(false)
     
     const { search } = useLocation();
 
@@ -20,7 +20,6 @@ export function VerifyEmail(){
 
     async function verifyEmail(){
        try {
-            setBlock(false)
             const emails = localStorage.getItem('emails') as unknown as string[];
            
             await fetch(`https://api-todo-oe5w.onrender.com/api/users/verify-email?email=${email}&token=${token}`, {
@@ -54,6 +53,7 @@ export function VerifyEmail(){
        }
     }
     if(!block){
+        setBlock(true)
         verifyEmail();
     }
 
@@ -68,6 +68,8 @@ export function VerifyEmail(){
             if(emails.includes(email) || !token){
                 navigate("/login")
             }
+
+            setBlock(false)
         }
         verifyRouter();
 
