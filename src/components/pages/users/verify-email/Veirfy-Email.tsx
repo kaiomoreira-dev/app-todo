70b5fc47e7d471c47f974eb23a1345da25892db5
Kaio Moreira <kaio-dev@outlook.com>
2023-10-20 14:59:58 -0300

    alter view for verify for block

--- src/components/pages/users/verify-email/Veirfy-Email.tsx
+++ src/components/pages/users/verify-email/Veirfy-Email.tsx
@@ -82,7 +82,7 @@ export function VerifyEmail(){
     // }
 
     return(
-        <div className={block ? styles.container : styles.none}>
+        <div className={block ? styles.none: styles.container}>
             <Header />
             <div className={styles['verification-content']}>
                 <img src={verificationImg} alt="" />
